22b9001639bcf92a721ca12f999e14f22d80ce84
Fung-Nkoh-Manuel <153408741+Fung-Nkoh-Manuel@users.noreply.github.com>
2026-01-06 23:59:14 +0100

    adding the file structure

---
+++ config/Database.php
@@ -0,0 +1,31 @@
+<!-- FILE: config/Database.php -->
+<?php
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'erp_system';
+    private $user = 'root';
+    private $password = '';
+    private $conn;
+
+    public function connect() {
+        $this->conn = new mysqli($this->host, $this->user, $this->password, $this->db_name);
+        
+        if ($this->conn->connect_error) {
+            die('Connection Error: ' . $this->conn->connect_error);
+        }
+        
+        $this->conn->set_charset("utf8mb4");
+        return $this->conn;
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+
+    public function closeConnection() {
+        if ($this->conn) {
+            $this->conn->close();
+        }
+    }
+}
+?>
